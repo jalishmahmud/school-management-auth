10a23e00554e78da99523c007049e8c52b4600ae
Jalish Mahmud <jalish@shellbeehaken.com>
2024-01-17 09:42:23 +0600

    setup cors and parser

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,15 @@
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+
 const app: Application = express();
+app.use(cors());
 
+//parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/", (req: Request, res: Response) => {
+  res.send("Working well!");
 });
 
 export default app;
