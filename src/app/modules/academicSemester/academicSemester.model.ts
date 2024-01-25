0b8b7cf682a32f4f72189cd41d1855f60bc6709a
jalishmahmud <jalish93@gmail.com>
2024-01-25 21:43:04 +0600

    restrict users from adding duplicate semester in same year

--- src/app/modules/academicSemester/academicSemester.model.ts
+++ src/app/modules/academicSemester/academicSemester.model.ts
@@ -1,4 +1,5 @@
 import { Schema, model } from 'mongoose'
+import status from 'http-status'
 import {
   AcademicSemesterModel,
   IAcademicSemester,
@@ -8,6 +9,7 @@ import {
   academicSemesterMonths,
   academicSemesterTitles,
 } from './academicSemester.constant'
+import ApiError from '../../../errors/ApiError'
 
 const academicSemesterSchema = new Schema<IAcademicSemester>(
   {
@@ -40,6 +42,18 @@ const academicSemesterSchema = new Schema<IAcademicSemester>(
     timestamps: true,
   },
 )
+
+academicSemesterSchema.pre('save', async function (next) {
+  const isExist = await AcademicSemester.findOne({
+    title: this.title,
+    year: this.year,
+  })
+  if (isExist) {
+    throw new ApiError(status.CONFLICT, 'Academic semester is already exist !')
+  } else {
+    next()
+  }
+})
 
 export const AcademicSemester = model<IAcademicSemester, AcademicSemesterModel>(
   'academicSemester',
