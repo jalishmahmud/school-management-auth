aa4c590d4275b58519e30e24ec886849b17e5fd5
jalishmahmud <jalish93@gmail.com>
2024-01-25 21:39:55 +0600

    academic semester service, controller, route

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import express, { Application } from 'express'
 import cors from 'cors'
 import globalErrorHandler from './app/middlewares/globalErrorHandler'
 import { UserRoutes } from './app/modules/users/user.route'
+import { SemesterRoutes } from './app/modules/academicSemester/academicSemester.route'
 
 const app: Application = express()
 app.use(cors())
@@ -13,6 +14,7 @@ app.use(express.urlencoded({ extended: true }))
 
 // Application routes
 app.use('/api/v1/users/', UserRoutes)
+app.use('/api/v1/academic-semesters/', SemesterRoutes)
 
 // testing
 // app.get('/', (req: Request, res: Response, next: NextFunction) => {
---
+++ src/app/modules/academicSemester/academicSemester.route.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+import { AcademicSemesterValidation } from './academicSemester.validation'
+import validateRequest from '../../middlewares/validateRequest'
+import { AcademicSemesterController } from './academicSemester.controller'
+const router = express.Router()
+router.post(
+  '/create-semester',
+  validateRequest(AcademicSemesterValidation.academicSemesterZodSchema),
+  AcademicSemesterController.createSemester,
+)
+
+export const SemesterRoutes = router
