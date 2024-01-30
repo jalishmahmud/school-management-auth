4b365a4f37a439ac4507f428c5d1490ccde95166
jalishmahmud <jalish93@gmail.com>
2024-01-30 23:48:00 +0600

    catchAsync and sendResponse handler for clean code

--- src/app/modules/users/user.controller.ts
+++ src/app/modules/users/user.controller.ts
@@ -1,18 +1,23 @@
-import { RequestHandler } from 'express'
+import { Request, Response, NextFunction } from 'express'
 import { UserService } from './user.service'
-const createUser: RequestHandler = async (req, res, next) => {
-  try {
+import catchAsync from '../../../shared/catchAsync'
+import sendResponse from '../../../shared/sendResponse'
+import httpStatus from 'http-status'
+
+const createUser = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
     const { user } = req.body
 
     const result = await UserService.createUser(user)
-    res.status(200).json({
+
+    next()
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
       success: true,
       message: 'User created successfully',
       data: result,
     })
-  } catch (error) {
-    next(error)
-  }
-}
+  },
+)
 
 export const UserController = { createUser }
